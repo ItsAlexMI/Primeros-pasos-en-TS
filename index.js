ae953b45271ce816b7c2e695d64accad54b49f52
Orrv2904 <omarrv2904@gmail.com>
2023-05-26 14:58:30 -0600

    Primeros pasos en TS

---
+++ index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var mensajeElement = document.getElementById("mensaje");
+    mensajeElement.innerHTML = "Hola Mundo!";
+});
---
+++ index.ts
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var mensajeElement = document.getElementById("mensaje");
+    mensajeElement.innerHTML = "Hola Mundo!";
+  });
+  
